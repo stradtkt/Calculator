f14c39c72a948d2a1d85efc49e2b8a56d3808f54
Kevin <stradtkt22@gmail.com>
2019-08-19 23:46:38 -0400

    Added layout and some of functionality

--- app/src/main/java/com/stradtman/calculator/MainActivity.java
+++ app/src/main/java/com/stradtman/calculator/MainActivity.java
@@ -1,13 +1,67 @@
 package com.stradtman.calculator;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText result;
+    private EditText newNumber;
+    private TextView displayOperations;
 
+    private Double operand1 = null;
+    private Double operand2 = null;
+    private String pendingOperation = "=";
+
+    @SuppressLint("CutPasteId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        result = (EditText)findViewById(R.id.result);
+        newNumber = (EditText)findViewById(R.id.result2);
+        displayOperations = (TextView)findViewById(R.id.result2);
+        Button button0 = (Button)findViewById(R.id.button0);
+        Button button1 = (Button)findViewById(R.id.button1);
+        Button button2 = (Button)findViewById(R.id.button2);
+        Button button3 = (Button)findViewById(R.id.button3);
+        Button button4 = (Button)findViewById(R.id.button4);
+        Button button5 = (Button)findViewById(R.id.button5);
+        Button button6 = (Button)findViewById(R.id.button6);
+        Button button7 = (Button)findViewById(R.id.button7);
+        Button button8 = (Button)findViewById(R.id.button8);
+        Button button9 = (Button)findViewById(R.id.button9);
+
+        Button buttonc = (Button)findViewById(R.id.buttonc);
+        Button buttonequal = (Button)findViewById(R.id.buttonequal);
+        Button buttonp = (Button)findViewById(R.id.buttonp);
+        Button buttonm = (Button)findViewById(R.id.buttonm);
+        Button buttonadd = (Button)findViewById(R.id.buttonadd);
+        Button buttondivide = (Button)findViewById(R.id.buttond);
+        Button buttondot = (Button)findViewById(R.id.buttondot);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Button b = (Button) view;
+                newNumber.append(b.getText().toString());
+            }
+        };
+        button0.setOnClickListener(listener);
+        button1.setOnClickListener(listener);
+        button2.setOnClickListener(listener);
+        button3.setOnClickListener(listener);
+        button4.setOnClickListener(listener);
+        button5.setOnClickListener(listener);
+        button6.setOnClickListener(listener);
+        button7.setOnClickListener(listener);
+        button8.setOnClickListener(listener);
+        button9.setOnClickListener(listener);
+
     }
 }
